b9dab7f6bea6a24795f5f68494e58c6bbc460c69
jkjode <104823459+jkjode@users.noreply.github.com>
2022-10-20 20:03:25 +0200

    1-4+6 finished

--- js/script.js
+++ js/script.js
@@ -13,57 +13,74 @@ const cats = [
 ];
 
 
-//Question 1:
-//Create an object called cat.
-//Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".
+//Question 1: (Finished?)
 
-let cat = new Object();
+const cat = {
+    complain: complain(),
+    }
 
-// cat(complain) {
-//     function
-//     console.log("Meow!");
-// }
+function complain() {
+    console.log("Meow!")
+} 
 
-//Question 2:
-//Select the h3 from the HTML using the querySelector method and assign it to a variable called heading.
-//Change its innerHTML value to "Updated heading".
 
-var heading = document.querySelector('h3');
+//Question 2: (Finished)
 
-heading.innerHTML = `<h3>Updated heading</h3>`;
+var heading = document.querySelector("h3");
 
-//Question 3:
-//Use the style property on the heading variable from the question above to change its font size to "2em".
+heading.innerHTML = "Updated heading";
+
+
+//Question 3: (Finished)
 
 heading.style.fontSize = "2em";
 
-//Question 4:
-//Add a class to the heading variable called subheading.
+//Question 4: (Finished)
 
-heading.addClass = ".subheading"
+heading.classList.add("subheading");
 
 //Question 5:
-//Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
-//Loop through the p elements and change the colour of each to "red".
+/*Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
+Loop through the p elements and change the colour of each to "red".*/
 
 var paragraphs = document.getElementsByTagName('p');
 
-// paragraphs.forEach(element => {
-//     element.style.color = 'red'
-// });
+/* paragraphs.forEach(element => {
+     element.style.color = 'red'
+ });*/
 
-//Question 6:
-//Select the div with a class of results, assign it to a variable called resultsContainer and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.
 
-var resultsContainer = document.getElementsByClassName('.results');
+//Question 6: (Finished)
+
+var resultsContainer = document.querySelector(".results");
 
 resultsContainer.innerHTML = '<p>Newparagraph</p>';
 resultsContainer.style.backgroundColor = "yellow";
 
 //Question 7:
-//Create a function that has one parameter called list.
-//Inside the function, loop through the list parameter and console log the name property in each object.
-//Call the function and pass in the cats variable in the script.js file in the repo.
+/*Create a function that has one parameter called list.
+Inside the function, loop through the list parameter and console log the name property in each object.
+Call the function and pass in the cats variable in the script.js file in the repo.*/
 
-var this = "hello world"
-console.log(this);
+
+cats.forEach(function (arrayItem) {
+    var list = arrayItem.prop1;
+    console.log(list)
+})
+
+
+cats.forEach(function(name) {
+   // console.log(name);
+})
+
+
+//Question 8:
+/*Create a function called createCats. The function will have one parameter called cats.
+Inside the function loop through the value passed in as cats and create HTML for each object in the array.
+Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
+If the age property is missing, it should display “Age unknown” instead.
+Return the HTML from the function.
+Call the function and pass in the cats array as the argument.
+Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.*/
+
+
