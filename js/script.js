a8044bcda9d1dc0f852adcc2f0ff57e72e1b8e8e
jkjode <104823459+jkjode@users.noreply.github.com>
2022-10-21 18:24:07 +0200

    Finished

--- js/script.js
+++ js/script.js
@@ -1,86 +1,86 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
 
-
-//Question 1: (Finished?)
+//Question 1:
 
 const cat = {
-    complain: complain(),
-    }
+  complain: complain(),
+};
 
 function complain() {
-    console.log("Meow!")
-} 
+  console.log("Meow!");
+}
 
-
-//Question 2: (Finished)
+//Question 2:
 
 var heading = document.querySelector("h3");
 
 heading.innerHTML = "Updated heading";
 
-
-//Question 3: (Finished)
+//Question 3:
 
 heading.style.fontSize = "2em";
 
-//Question 4: (Finished)
+//Question 4:
 
 heading.classList.add("subheading");
 
 //Question 5:
-/*Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
-Loop through the p elements and change the colour of each to "red".*/
 
-var paragraphs = document.getElementsByTagName('p');
+var paragraphs = document.querySelectorAll("p");
 
-/* paragraphs.forEach(element => {
-     element.style.color = 'red'
- });*/
+function changeColor(newcolor) {
+  newcolor.style.color = "red";
+}
 
+for (var i = 0; i < paragraphs.length; i++) {
+  changeColor(paragraphs[i]);
+}
 
-//Question 6: (Finished)
+//Question 6:
 
 var resultsContainer = document.querySelector(".results");
 
-resultsContainer.innerHTML = '<p>Newparagraph</p>';
+resultsContainer.innerHTML = "<p>New paragraph</p>";
 resultsContainer.style.backgroundColor = "yellow";
 
 //Question 7:
-/*Create a function that has one parameter called list.
-Inside the function, loop through the list parameter and console log the name property in each object.
-Call the function and pass in the cats variable in the script.js file in the repo.*/
 
+function listOfCats(list) {
+  for (var i = 0; i < list.length; i++) {
+    console.log(list[i].name);
+  }
+}
 
-cats.forEach(function (arrayItem) {
-    var list = arrayItem.prop1;
-    console.log(list)
-})
-
-
-cats.forEach(function(name) {
-   // console.log(name);
-})
-
+listOfCats(cats);
 
 //Question 8:
-/*Create a function called createCats. The function will have one parameter called cats.
-Inside the function loop through the value passed in as cats and create HTML for each object in the array.
-Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
-If the age property is missing, it should display “Age unknown” instead.
-Return the HTML from the function.
-Call the function and pass in the cats array as the argument.
-Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.*/
 
+const catContainer = document.querySelector(".cat-container");
 
+function createCats(cats) {
+  const newHTML = [];
+  cats.forEach((cat) => {
+    if (cat.age) {
+      const HTML = `<div><h5>${cat.name}</h5><p>${cat.age}</p></div>`;
+      newHTML.push(HTML);
+    } else {
+      const HTML = `<div><h5>${cat.name}</h5><p>Age unknown<p></div>`;
+      newHTML.push(HTML);
+    }
+  });
+  return newHTML;
+}
+
+catContainer.innerHTML = createCats(cats);
